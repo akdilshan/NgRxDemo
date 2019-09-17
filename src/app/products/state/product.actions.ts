5fdae9e86d3ed04cb57e4f510d518749130864b1
Dilshan <dilshanamarasinghe1@gmail.com>
2019-09-18 07:26:57 +0800

    Added typed syntax for Stores, and implement selectors

---
+++ src/app/products/state/product.actions.ts
@@ -0,0 +1,11 @@
+import { Action } from '@ngrx/store';
+
+export enum ProductActionTypes {
+    ToggleProductCode= '[Product] Toggle Product Code'
+}
+
+export class ToggleProductCode implements Action {
+readonly type = ProductActionTypes.ToggleProductCode;
+
+constructor(public payload: boolean) {}
+}
--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,4 +1,25 @@
-export function reducer(state, action) {
+import {User} from '../user';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+export interface UserState {
+    maskUserName: boolean;
+    currentUser: User;
+}
+
+
+const InitialState: UserState = {
+    maskUserName: true,
+    currentUser: null
+};
+
+const getUserFeatureState = createFeatureSelector<UserState>('users');
+
+export const getMaskUserName = createSelector(
+    getUserFeatureState,
+    state => state.maskUserName
+);
+
+export function reducer(state = InitialState, action): UserState {
 
     switch (action.type) {
         case 'MASK_USER_NAME':
